d8a763b498270d66b9c4ed94d4a0bc667e9bf951
guus_vrv <guus@guusvanrees.com>
2025-09-26 09:38:09 +0200

    moved quote

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/generate',
+          permanent: true, // use false if you might change later
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
